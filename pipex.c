aea59b726a91dc8e897ae4e5b12815b1148cfaa4
Allen Yajirobe <ayajirob@oa-c1.msk.21-school.ru>
2021-12-16 15:22:03 +0300

    done without norminet

--- pipex.c
+++ pipex.c
@@ -5,9 +5,60 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 
+char	*ft_parsing(int argc, char **argv, int *here_doc, int *process_num, int *cmd_index)
+{
+	char	*stop_value;
+
+	if (argc < 5)
+	{
+		ft_putstr_fd("The number of arguments should be at least 4\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	*here_doc = 0;
+	*cmd_index = 2;
+	*process_num = argc - 3;
+	stop_value = NULL;
+	if (ft_strncmp(argv[1], "here_doc", ft_strlen(argv[1])) == 0)
+	{
+		*here_doc = 1;
+		*cmd_index = 3;
+		*process_num = argc - 4;
+		stop_value = ft_strjoin(argv[2], "\n");
+	}
+	return (stop_value);
+}
+
+void	ft_cmd_parser(int cmd_index, char ***array, char **path, int argc, char **argv)
+{
+	int	y;
+
+	y = 0;
+	while (cmd_index < argc - 1)
+	{
+		array[y] = ft_split(argv[cmd_index], ' ');
+		if (array[y] == NULL)
+			perror("Malloc failed: ");
+		path[y] = ft_strjoin("/usr/bin/", *array[y]);
+		if (path[y] == NULL)
+			perror("Malloc failed: ");
+		if (access(path[y], F_OK) == -1)
+		{
+			free(path[y]);
+			path[y] = NULL;
+			path[y] = ft_strjoin("/bin/", *array[y]);
+			if (path[y] == NULL)
+				perror("Malloc failed: ");
+			if (access(path[y], F_OK) == -1)
+				perror("Access failed");
+		}
+		cmd_index++;
+		y++;
+	}
+}
+
 int	**ft_create_pipes(int process_num, int **pipes)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < process_num + 1)
@@ -52,20 +103,18 @@ void	ft_close_unused_pipes(int i, int **pipes, int process_num, char c)
 	}
 }
 
-void	ft_read_in_loop(int in_fd, int out_fd, int here_doc, char *stop_value)
+void ft_read_in_loop(int in_fd, int out_fd, int here_doc, char *stop_value)
 {
-	char	*line;
-	int		stop_flag;
+	char *line;
+	int stop_flag;
 
 	stop_flag = 1;
 	line = malloc(1);
-	//printf("%s\n", stop_flag);
 	while (line && stop_flag)
 	{
 		free(line);
 		line = NULL;
 		line = get_next_line(in_fd);
-		printf("%d\n",ft_strncmp(line, stop_value, ft_strlen(line)));
 		if (here_doc == 0 && line != NULL)
 		{
 			if (write(out_fd, line, ft_strlen(line)) == -1)
@@ -91,71 +140,79 @@ void	ft_read_in_loop(int in_fd, int out_fd, int here_doc, char *stop_value)
 		perror("Close failed");
 }
 
+void	ft_read_input(int here_doc, char *infile, int first_pipe, char *stop_value)
+{
+	int	input_fd;
+
+	if (here_doc == 1)
+		input_fd = 0;
+	else
+		input_fd = open(infile, O_RDONLY);
+	if (input_fd == -1)
+	{
+		perror("");
+		exit(EXIT_FAILURE);
+	}
+	ft_read_in_loop(input_fd, first_pipe, here_doc, stop_value);
+}
+
+void	ft_write_output(int here_doc, char *outfile, int last_pipe)
+{
+	int	output_fd;
+
+	if (here_doc == 1)
+	{
+		output_fd = open(outfile, O_RDWR | O_APPEND | O_CREAT, 0644);
+		here_doc = 0;
+	}
+	else
+		output_fd = open(outfile, O_RDWR | O_TRUNC | O_CREAT, 0644);
+	if (output_fd == -1)
+	{
+		perror("");
+		exit(EXIT_FAILURE);
+	}
+	ft_read_in_loop(last_pipe, output_fd, here_doc, NULL);
+}
+
+void	ft_waiting_childs(int process_num)
+{
+	int	i;
+
+	i = 0;
+	while (i < process_num)
+	{
+		wait(NULL);
+		i++;
+	}
+	exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv)
 {
-	int **pipes;
-	int i;
-	int x;
-	int y;
-	int n;
-	int process_num;
-	int pids[argc - 3];
+	int		**pipes;
+	int		i;
+	int		cmd_index;
+	int		process_num;
+	int		pids[argc - 3];
 	char	**path;
 	char	***cmd_array;
 	char	***array;
-	int 	input_fd;
-	int 	output_fd;
-	int 	here_doc;
+	int		here_doc;
 	char	*stop_value;
 
-	if (argc < 5)
-	{
-		ft_putstr_fd("The number of arguments should be at least 4\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	here_doc = 0;
-	x = 2;
-	process_num = argc - 3;
-	if (ft_strncmp(argv[1], "here_doc", ft_strlen(argv[1])) == 0)
-	{
-		printf("It's here_doc\n");
-		here_doc = 1;
-		x = 3;
-		process_num = argc - 4;
-		stop_value = ft_strjoin(argv[2], "\n");
-	}
+	stop_value = ft_parsing(argc, argv, &here_doc, &process_num, &cmd_index);
 	array = (char ***)malloc(process_num * sizeof(char **));
+	if (array == NULL)
+		perror("Malloc failed: ");
 	path = (char **)malloc((process_num) * sizeof(char *));
+	if (path == NULL)
+		perror("Malloc failed: ");
 	pipes = (int **)malloc((process_num + 1) * sizeof(int *));
+	if (pipes == NULL)
+		perror("Malloc failed: ");
 	cmd_array = array;
-	y = 0;
-	while(x < argc - 1)
-	{
-		array[y] = ft_split(argv[x], ' ');
-		path[y] = ft_strjoin("/usr/bin/", *array[y]);
-		if (access(path[y], F_OK) == -1)
-		{
-			free(path[y]);
-			path[y] = NULL;
-			path[y] = ft_strjoin("/bin/", *array[y]);
-			if (access(path[y], F_OK) == -1)
-			{
-				perror("Access failed");
-				return (0);
-			}
-		}
-		x++;
-		y++;
-	}
-	printf("%s\n", path[0]);
-	printf("%s\n", path[1]);
-	//printf("%s\n", path[2]);
-	printf("%s\n", cmd_array[0][0]);
-	printf("%s\n", cmd_array[0][1]);
-	printf("%s\n", cmd_array[1][0]);
-	printf("%s\n", cmd_array[1][1]);
-	//printf("%s\n", cmd_array[2][0]);
-	//printf("%s\n", cmd_array[2][1]);
+	ft_cmd_parser(cmd_index, array, path, argc, argv);
 	pipes = ft_create_pipes(process_num, pipes);
 	i = 0;
 	while (i < process_num)
@@ -170,49 +227,22 @@ int main(int argc, char **argv)
 		{
 			ft_close_unused_pipes(i, pipes, process_num, 'c');
 			dup2(pipes[i][0], 0);
-			dup2(pipes[i+1][1], 1);
-			n = execve(*path, *cmd_array, (void *)0);
-			if (n == -1)
+			dup2(pipes[i + 1][1], 1);
+			if (execve(*path, *cmd_array, (void *)0) == -1)
 			{
 				perror("Execve failed");
 				exit(EXIT_FAILURE);
 			}
 			close(pipes[i][0]);
-			close(pipes[i + 1][1]);
-			return (0);
+			close(pipes[i++ + 1][1]);
+			exit(EXIT_SUCCESS);
 		}
 		cmd_array++;
 		path++;
 		i++;
 	}
 	ft_close_unused_pipes(i, pipes, process_num, 'p');
-	if (here_doc == 1)
-	{
-		input_fd = 0;
-	}
-	else
-		input_fd = open(argv[1], O_RDONLY);
-	if (input_fd == -1)
-	{
-		perror("");
-		exit(EXIT_FAILURE);
-	}
-	ft_read_in_loop(input_fd, pipes[0][1], here_doc, stop_value);
-
-	if (here_doc == 1)
-	{
-		output_fd = open(argv[argc-1], O_RDWR | O_APPEND | O_CREAT, 0644);
-		here_doc = 0;
-	}
-	else
-		output_fd = open(argv[argc-1], O_RDWR | O_TRUNC | O_CREAT, 0644);
-	ft_read_in_loop(pipes[process_num][0], output_fd, here_doc, argv[2]);
-
-	i = 0;
-	while (i < process_num)
-	{
-		wait(NULL);
-		i++;
-	}
-	return(0);
+	ft_read_input(here_doc, argv[1], pipes[0][1], stop_value);
+	ft_write_output(here_doc, argv[argc - 1], pipes[process_num][0]);
+	ft_waiting_childs(process_num);
 }
